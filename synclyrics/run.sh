98f1cfd0882037acf1f152c458adbcb9a9ff14db
Anshul <anshulaj999@gmail.com>
2025-11-23 16:50:11 +0530

    Fixes 4

--- synclyrics/run.sh
+++ synclyrics/run.sh
@@ -1,18 +1,27 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
+set -e
 
 echo "Starting SyncLyrics..."
 
-# Read config from HA options
-export SPOTIFY_CLIENT_ID=$(bashio::config 'spotify_client_id')
-export SPOTIFY_CLIENT_SECRET=$(bashio::config 'spotify_client_secret')
-export SPOTIFY_REDIRECT_URI=$(bashio::config 'spotify_redirect_uri')
-export SPOTIFY_BASE_URL=$(bashio::config 'spotify_base_url')
-export SERVER_PORT=$(bashio::config 'server_port')
+# Path to Home Assistant Options
+CONFIG_PATH="/data/options.json"
+
+# Helper function to read config using jq
+function get_config {
+    jq --raw-output ".$1 // empty" $CONFIG_PATH
+}
+
+# Read config variables
+export SPOTIFY_CLIENT_ID=$(get_config 'spotify_client_id')
+export SPOTIFY_CLIENT_SECRET=$(get_config 'spotify_client_secret')
+export SPOTIFY_REDIRECT_URI=$(get_config 'spotify_redirect_uri')
+export SPOTIFY_BASE_URL=$(get_config 'spotify_base_url')
+export SERVER_PORT=$(get_config 'server_port')
 
 # Generate a random secret key for the session
 export QUART_SECRET_KEY="ha-secret-$(date +%s)"
 
-# Set other defaults for Linux/Docker
+# Set Linux defaults
 export DESKTOP="Linux"
 export PYTHONUNBUFFERED=1
 
