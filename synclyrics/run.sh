5368c9a87606d384a3dd8c2bf70805c795399482
Anshul <anshulaj999@gmail.com>
2025-11-23 16:32:56 +0530

    Fixes 2

--- synclyrics/run.sh
+++ synclyrics/run.sh
@@ -6,6 +6,7 @@ echo "Starting SyncLyrics..."
 export SPOTIFY_CLIENT_ID=$(bashio::config 'spotify_client_id')
 export SPOTIFY_CLIENT_SECRET=$(bashio::config 'spotify_client_secret')
 export SPOTIFY_REDIRECT_URI=$(bashio::config 'spotify_redirect_uri')
+export SPOTIFY_BASE_URL=$(bashio::config 'spotify_base_url')
 export SERVER_PORT=$(bashio::config 'server_port')
 
 # Generate a random secret key for the session
